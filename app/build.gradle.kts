062bdb07a6dee6722b83f9140cac60fc27056cf2
Michael Schättgen <michael@schattgen.me>
2026-05-19 20:27:04 +0200

    Bump version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "me.schattgen.felis"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
